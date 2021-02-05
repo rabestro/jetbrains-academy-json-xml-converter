58e94a23ad0f10fd004462347934b7d05fea5607
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-05 23:13:58 +0200

    Stage solved

--- JSON - XML converter/task/src/converter/Main.java
+++ JSON - XML converter/task/src/converter/Main.java
@@ -1,7 +1,39 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final var scanner = new Scanner(System.in);
+        final var converter = new Converter(scanner.nextLine());
+
+        System.out.println(converter.isJson() ? converter.toXml() : converter.toJson());
+    }
+
+}
+
+class Converter {
+    private final String data;
+
+    Converter(String data) {
+        this.data = data;
+    }
+
+    boolean isJson() {
+        return data.startsWith("{");
+    }
+
+    String toJson() {
+        if (data.endsWith("/>")) {
+            return data.replaceFirst("<(\\w+)/>", "{\"$1\":null}");
+        }
+        return data.replaceFirst("<(\\w+)>(.+)</\\1>", "{\"$1\":\"$2\"}");
+    }
+
+    String toXml() {
+        if (data.endsWith(":null}")) {
+            return data.replaceFirst("\\{\"(\\w+)\":null}", "<$1/>");
+        }
+        return data.replaceFirst("\\{\"(\\w+)\":\"(.+)\"}","<$1>$2</$1>");
     }
 }

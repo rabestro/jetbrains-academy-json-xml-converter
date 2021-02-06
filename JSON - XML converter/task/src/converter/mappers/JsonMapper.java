bc00678c718675ae4b9647679f214ccc43670bdd
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-06 23:27:52 +0200

    Add JsonMapper

---
+++ JSON - XML converter/task/src/converter/mappers/JsonMapper.java
@@ -0,0 +1,10 @@
+package converter.mappers;
+
+import converter.domain.Element;
+
+public class JsonMapper implements ObjectMapper {
+    @Override
+    public Element read(String data) {
+        return null;
+    }
+}

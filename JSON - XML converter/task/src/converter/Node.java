de9a9d6fccc016edaa7182727ef1f5d90952e6a6
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-06 20:06:39 +0200

    Add Node

---
+++ JSON - XML converter/task/src/converter/Node.java
@@ -0,0 +1,11 @@
+package converter;
+
+import java.util.List;
+import java.util.Map;
+
+public class Node {
+    private String name;
+    private Map<String, String> attributes;
+    private String value;
+    private List<Node> child;
+}
---
+++ JSON - XML converter/task/src/converter/XmlMapper.java
@@ -0,0 +1,11 @@
+package converter;
+
+public class XmlMapper {
+    public Node read(final String data) {
+        return null;
+    }
+
+    public String toJson() {
+        return "";
+    }
+}

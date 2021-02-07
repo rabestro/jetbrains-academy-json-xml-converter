2d97349b183340845d920f544c0ca3a433c68874
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-07 10:36:08 +0200

    Added method DocumentPrinter::print

--- JSON - XML converter/task/src/converter/service/DocumentPrinter.java
+++ JSON - XML converter/task/src/converter/service/DocumentPrinter.java
@@ -1,4 +1,15 @@
 package converter.service;
 
+import converter.document.Element;
+
 public class DocumentPrinter {
+    private final Element document;
+
+    public DocumentPrinter(Element document) {
+        this.document = document;
+    }
+
+    public void print() {
+
+    }
 }

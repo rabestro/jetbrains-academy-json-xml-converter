ac0a7f5a1987591d3295e52ded1fc1487f321ab0
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-20 15:19:35 +0200

    Updated code for stage 1

--- JSON - XML converter/task/src/converter/mapper/ObjectMapper.java
+++
@@ -1,9 +0,0 @@
-package converter.mapper;
-
-import converter.domain.Element;
-
-public interface ObjectMapper {
-    Element read(final String data);
-
-    String write(final Element document);
-}
